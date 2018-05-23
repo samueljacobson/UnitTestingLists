c75b7ea159f82aeb49a24ce66bb4cf8962786ea2
samueljacobson <samuel.c.jacobson@gmail.com>
2018-05-23 15:32:49 -0400

    completed problem set

---
+++ ListsHomework/ListsTests.cs
@@ -0,0 +1,111 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace ListsHomework
+{
+    [TestFixture]
+    public class ListsTests
+    {
+        // Whenever you see this constant, you'll need to delete the
+        // variable reference and replace it with a literal value.
+        const object __FILL_ME_IN__ = null;
+
+        [Test]
+        public void Lists_Are_Empty_By_Default()
+        {
+            var list = new List<int>();
+
+            // Replace __FILL_ME_IN__ with the correct value.
+            Assert.That(list.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Lists_Can_Be_Initialized_With_Items()
+        {
+            var list = new List<int>()
+            {
+                // Add 3 items to the list here.
+                1, 2, 3
+            };
+
+            Assert.That(list.Count, Is.EqualTo(3));
+        }
+
+        [Test]
+        public void Lists_Can_Have_Items_Added_After_They_Are_Created()
+        {
+            var list = new List<string>();
+
+            // Use one of List's methods to add an item here.
+            list.Add("foo");
+            list.Add("bar");
+            list.Add("baz");
+
+            Assert.That(list, Is.Not.Empty);
+        }
+
+        [Test]
+        public void Lists_Use_Zero_Based_Indexes()
+        {
+            var list = new List<string>() { "Foo", "Bar", "Baz" };
+
+            int indexOfSecondItem = list.IndexOf("Bar");
+
+            Assert.That(indexOfSecondItem, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Lists_Can_Have_Items_Inserted_At_Specific_Locations()
+        {
+            var list = new List<int>() { 1, 3 };
+
+            // Use one of List's methods to insert the number 2
+            // in the correct position here.
+            list.Insert(1, 2);
+
+            CollectionAssert.AreEqual(list, new List<int> { 1, 2, 3 });
+        }
+
+        [Test]
+        public void Lists_Can_Have_Items_Removed_At_A_Specific_Index()
+        {
+            var list = new List<int>() { 1, 2, 3, 4 };
+
+            // Use one of List's methods to delete the item at index 2.
+            list.RemoveAt(2);
+
+            CollectionAssert.AreEqual(list, new List<int> { 1, 2, 4 });
+        }
+
+        [Test]
+        public void Lists_Can_Have_Specific_Values_Removed()
+        {
+            var list = new List<int>() { 1, 2, 42, 3 };
+
+            // Use one of List's methods to delete the value 42.
+            list.Remove(42);
+
+            CollectionAssert.AreEqual(list, new List<int> { 1, 2, 3 });
+        }
+
+        [Test]
+        public void Lists_Allow_You_To_Set_Values_With_Indexers()
+        {
+            var list = new List<string>() { "Foo", "Bar", "Baz" };
+
+            // Use an indexer (square brackets) to change "Bar" to "Quux".
+            list[1] = "Quux";
+
+            CollectionAssert.AreEqual(list, new List<string> { "Foo", "Quux", "Baz" });
+        }
+
+        [Test]
+        public void Lists_Allow_You_To_Get_Values_With_Indexers()
+        {
+            var list = new List<string>() { "Foo", "Bar", "Baz" };
+
+            // What value does list[2] return?
+            Assert.That(list[2], Is.EqualTo("Baz"));
+        }
+    }
+}
